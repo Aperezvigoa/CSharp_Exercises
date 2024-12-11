4ad9917083b5ff4fa448cc0c2fa03b51afb41a32
Alejandro Pérez-Vigo Albizu <alejandroperezvigo@outlook.com>
2024-12-11 21:30:41 +0100

    Cleaned submodule references and re-added projects as folders

---
+++ Beginner-001_OOP_Dice_Game_SRP/OOP_Dice_game/DiceGame.cs
@@ -0,0 +1,20 @@
+﻿var dice = new Dice(6);
+var UserInput = new DiceInputHandler();
+var player = new PlayerState();
+var PlayingGame = new DiceGame();
+
+PlayingGame.RunningGame(dice, UserInput, player);
+Console.ReadKey();
+public class DiceGame
+{
+    public void RunningGame(Dice GeneratedDiceNumber, DiceInputHandler ActualUserInput, PlayerState newPlayer)
+    {
+        Console.WriteLine($"Dice rolled. Guess what number it shows in {PlayerState.MaxTries} tries.");
+        while (!newPlayer.GameEnd)
+        {
+            Console.WriteLine($"remaining attempts: {PlayerState.MaxTries - ActualUserInput.CountOfTries}");
+            ActualUserInput.NumberValidation(GeneratedDiceNumber);
+            newPlayer.PlayerCondition(GeneratedDiceNumber, ActualUserInput);
+        }
+    }
+}
---
+++ Beginner-002_OOP_Hangman_Game/Hangman/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+👨‍💻 Developed by Alejandro Pérez-Vigo Albizu (GitHub => Aperezvigoa)
+
+This project was created for educational purposes and as a programming exercise.
+Feel free to explore the code, learn from it, and make modifications for your own educational purposes.
+
+The goal is to share knowledge and promote collaborative learning, so if you have ideas, improvements, 
+or just want to practice, go ahead!
+
+📂 Open Source for the Community
+
+Note: If you use this project as a base for something else, it would be great if you could give credit to the original developer. 😊
+
+Thank you for checking it out! 🚀
+*/
+
+
+using Hangman.Game;
+
+var game = new Hangman.Game.Hangman();
+game.RunGame();
+
+Console.ReadKey();
